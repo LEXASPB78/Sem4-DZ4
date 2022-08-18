459cfdd25d6baf50e47eda189c710f3a6a3c8ebf
LEXASPB78 <ashkreba@yandex.ru>
2022-08-18 22:41:37 +0300

    Reshenie27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void PrintResult(int a)
+{
+    Console.WriteLine($"Сумма цифр числа {a} равна: {SumNumberDigitStr(a)} (через строки)");
+    Console.WriteLine($"Сумма цифр числа {a} равна: {SumNumberDigit(a)} (цикличный поиск)");
+
+}
+
+int SumNumberDigitStr(int a)
+{
+    int result = 0;
+    int numberAbs = Convert.ToInt32(Math.Abs(a));
+    string numberAbsStr = numberAbs.ToString();
+    for (int i = 0; i < numberAbsStr.Length; i++)
+    {
+        //Console.WriteLine($"i={i};result={result}; a[i]={numberAbsStr[i]}; Char.GetNumericValue(numberAbsStr[i]) = {Char.GetNumericValue(numberAbsStr[i])}");
+        result = result + Convert.ToInt32(Char.GetNumericValue(numberAbsStr[i]));
+    }
+    return result;
+}
+
+int SumNumberDigit(int a)
+{
+    int numberAbs = Convert.ToInt32(Math.Abs(a));
+    int result = 0;
+    if (numberAbs <= 9)
+    { result += numberAbs % 10; }
+    else
+    //int result = numberAbs % 10;
+    //if (numberAbs > 9)
+    {
+        while (numberAbs / 10 != 0)
+        {
+            result += numberAbs % 10;
+            numberAbs /= 10;
+        }
+        result += numberAbs;
+    }
+    return result;
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+PrintResult(number);

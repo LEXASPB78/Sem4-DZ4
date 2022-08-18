e8dc97faa8e1868f70018500f559adb29a61254c
LEXASPB78 <ashkreba@yandex.ru>
2022-08-18 22:36:38 +0300

    Reshenie25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два 
+// числа (A и B) и возводит число A в натуральную степень B.
+
+int ReadData(string comm)
+{
+    Console.Write(comm);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int LocalPow(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+        result *= A;
+
+    return result;
+}
+
+int A = ReadData("Основание степени...");
+int B = ReadData("Показатель степени...");
+if (B < 0)
+{
+    Console.WriteLine("Показатель степени должен быть > 0");
+}
+else
+{
+    Console.WriteLine($"{A} в степени {B} = {LocalPow(A, B)}");
+
+}
